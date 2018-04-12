7e067361761b79a3826a1c374d902dc59f5a050e
Steven <calmrainbp@gmail.com>
2018-04-12 16:31:24 -0700

    edited KMapVC
    hooked up to apple maps, tracks the suers location
    
    need constrains, testing

--- Kwikkutz/Controller/KMapVC.swift
+++ Kwikkutz/Controller/KMapVC.swift
@@ -15,6 +15,16 @@ class KMapVC: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var map: MKMapView!
     let manager = CLLocationManager()
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         manager.delegate = self
